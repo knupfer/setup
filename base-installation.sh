cf691d2f6c48f473b5bb5e7fec50ba965a429815
knupfer <fknupfer@gmail.com>
2014-05-09 09:04:12 +0200

    remove sudo requirement, link emacs into root usr

--- base-installation.sh
+++ base-installation.sh
@@ -1,7 +1,7 @@
-echo "deb http://ftp.de.debian.org/debian testing main contrib non-free" > /etc/apt/sources.list
-apt-get update
-apt-get -y upgrade
-apt-get -y install i3 xdotool ipython emacs24-lucid emacs24-common-non-dfsg git python3 htop gnutls-bin gnucash texlive-full lilypond dmenu libreoffice iceweasel okular r-base gimp audacity w3m emms install-info lzop
+sudo echo "deb http://ftp.de.debian.org/debian unstable main contrib non-free" > /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install i3 xdotool ipython emacs24-lucid emacs24-common-non-dfsg git python3 htop gnutls-bin gnucash texlive-full lilypond dmenu libreoffice iceweasel okular r-base gimp audacity w3m emms install-info lzop aspell-de
 
 mkdir ~/git
 
@@ -17,11 +17,14 @@ echo "[user]
     email = fknupfer@gmail.com
     name = knupfer" > .gitconfig
     
-ln -s git/dotfiles/emacs/.emacs .emacs
-ln -s git/dotfiles/emacs/.emacs.d/ .emacs.d
-ln -s git/dotfiles/i3wm/.i3/ .i3
+ln -s ~/git/dotfiles/emacs/.emacs ~/.emacs
+ln -s ~/git/dotfiles/emacs/.emacs.d/ ~/.emacs.d
+ln -s ~/git/dotfiles/i3wm/.i3/ ~/.i3
 sh ~/.i3/change-mode.sh ACTIVE
 
-apt-get -y dist-upgrade
-apt-get clean
-apt-get -y autoremove
+sudo ln -s ~/.emacs /root/.emacs
+sudo ln -s ~/.emacs.d /root/.emacs.d
+sudo ln -s ~/.i3 /root/.i3
+sudo apt-get -y dist-upgrade
+sudo apt-get clean
+sudo apt-get -y autoremove

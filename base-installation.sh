3319dce54dba9deffbd5899c8d5754c2cd55712b
knupfer <fknupfer@gmail.com>
2014-08-06 14:55:23 +0200

    add lilypond-pretty-print

--- base-installation.sh
+++ base-installation.sh
@@ -3,12 +3,10 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install i3 xdotool ipython emacs24-lucid emacs24-common-non-dfsg git python3 htop gnutls-bin gnucash texlive-full lilypond dmenu libreoffice iceweasel okular r-base r-cran-ggplot2 gimp audacity w3m emms install-info lzop aspell-de ess aqbanking-tools x11-xkb-utils xfce4-terminal owncloud-client bash-completion mupdf locales-all
 
-mkdir ~/git
-
 cd ~/git
 git clone http://www.github.com/knupfer/dotfiles.git
 git clone http://www.github.com/knupfer/knupfer.github.io.git
-git clone http://www.github.com/knupfer/setup.git
+git clone http://www.github.com/knupfer/lilypond-pretty-print.git
 git clone http://www.github.com/knupfer/global-emacs.git
 git clone http://www.github.com/knupfer/indentation-tree.el.git
 git clone http://www.github.com/knupfer/macro-type.git
@@ -24,9 +22,6 @@ ln -s ~/git/dotfiles/i3wm/.i3/ ~/.i3
 ln -s ~/git/dotfiles/iceweasel/.pentadactylrc ~/.pentadactylrc
 sh ~/.i3/change-mode.sh ACTIVE
 
-sudo ln -s ~/.emacs /root/.emacs
-sudo ln -s ~/.emacs.d /root/.emacs.d
-sudo ln -s ~/.i3 /root/.i3
 sudo apt-get -y dist-upgrade
 sudo apt-get clean
 sudo apt-get -y autoremove
